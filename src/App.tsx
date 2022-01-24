2fd5be15b84dcaa9665b5beba93ef4a95c1dc2dc
hihumikan <musecharo@gmail.com>
2022-01-25 01:23:36 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
 
-function App() {
+import { Child1, Child2 } from "./components/Child";
+
+const App: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+  const handleIncrement = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
+  const handleDecrement = useCallback(() => {
+    setCount((prev) => prev - 1);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <div>{count}</div>
+        <div>
+          <button onClick={handleIncrement}>+1</button>
+          <button onClick={handleDecrement}>-1</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Child.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Child1: React.FC = () => {
+  return <div>子のコンポーネント1</div>;
+};
+
+export const Child2: React.FC = () => {
+  return <div>子のコンポーネント2</div>;
+};
